3e3802f1143ed4aeead0b9497e4123f58dde90cc
wibetter <365533093@qq.com>
2021-12-28 19:35:01 +0800

    fix: 线上示例代码改用线上资源根路径

--- akfun.config.js
+++ akfun.config.js
@@ -65,7 +65,7 @@ module.exports = {
     // 用于构建生产环境代码的相关配置信息
     NODE_ENV: 'production',
     assetsRoot: resolve('./test'), // 打包后的文件绝对路径（物理路径）
-    assetsPublicPath: '/amis-widget/test/', // 设置静态资源的引用路径（根域名+路径）
+    assetsPublicPath: 'https://aisuda.github.io/amis-widget/test/', // 设置静态资源的引用路径（根域名+路径）
     assetsSubDirectory: '', // 资源引用二级路径
     productionSourceMap: false,
     productionGzip: false,
