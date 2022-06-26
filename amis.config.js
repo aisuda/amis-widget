d08135686e8d8acb9b0869df90e7601a761441ba
wibetter <365533093@qq.com>
2022-06-26 11:45:46 +0800

    fix: update

--- amis.config.js
+++ amis.config.js
@@ -35,7 +35,7 @@ module.exports = {
     moduleRules: [], // 用于配置自定义loaders
     plugins: [] // 用于配置自定义plugins
   },
-  dev: {
+  dev1: {
     entry: { // 调试模式的入口1（使用构建后的amis-widget）
       index: [
         './editor/EditorDemo.jsx',
@@ -59,7 +59,7 @@ module.exports = {
     closeHotReload: false, // 是否关闭热更新
     closeEditorClient: true, // 是否关闭自动注入editor
   },
-  dev2: {
+  dev: {
     entry: { // 本地调试模式的入口2（使用本地的amis-widget）
       index: [
         './editor/EditorDemo.jsx',
